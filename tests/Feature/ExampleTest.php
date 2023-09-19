c9aa4a30d639a60467c512a6a6336a338f3ba08f
Helmy <helmy@fotia.com.my>
2023-09-19 10:11:17 +0800

    added 01_05b

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,30 +1,21 @@
 <?php
 
-use App\Models\User;
+namespace Tests\Feature;
 
-// regular test.
-it('the application returns a successful response', function () {
-    $this->get('/login')
-        ->assertStatus(200);
-});
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
-// higher order test. we don;t use a closure
-it('the application returns a successful response chained')
-    ->get('/login')
-    ->assertStatus(200);
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/');
 
-it('the application returns a successful response expectation', function () {
-    $response = $this->get('/login');
-    expect($response->getStatusCode())->toBe(200);
-});
-
-it('user is found and meets criteria expectations', function () {
-    $user = User::factory()->make([
-        'name' => 'Fernando',
-        'email' => 'fernando@email.com'
-    ]);
-
-    expect($user)
-        ->name->toBe('Fernando')
-        ->email->toContain('fernando');
-});
+        $response->assertStatus(200);
+    }
+}
